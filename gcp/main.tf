884cf860ee7d53c07b9e5d10452cd7254aaa39a5
Andrey Burtasov <a.burtasov@yarus.ru>
2022-07-10 23:27:19 +0300

    added gcp

---
+++ gcp/main.tf
@@ -0,0 +1,19 @@
+provider "google" {
+  credentials = file("mygcp-creds.json")
+  project = "project_id"
+  region = "us-wes1"
+  zone = "us-west1-b"
+}
+
+resource "google_compute_instance" "my_server" {
+  name = "my-gcp-server"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+        image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+}
