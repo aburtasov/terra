26e9681238e1e33acaf9c5720fd5dada81bfc473
Andrey Burtasov <a.burtasov@yarus.ru>
2022-07-11 06:36:58 +0300

    added recreate

---
+++ other/recreate.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "node1" {
+  ami = "ami-*"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Node-1"
+    Owner = "Me"
+  }
+}
+
+resource "aws_instance" "node2" {
+  ami = "ami-*"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Node-2"
+    Owner = "Me"
+  }
+}
+
+resource "aws_instance" "node3" {
+  ami = "ami-*"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Node-3"
+    Owner = "Me"
+  }
+
+  depends_on = [
+    aws_instance.node2
+  ]
+}
+
+// пересоздать node1 ... можно с помощью комментирования ресурса
+// кроме depends_on 
+
+// terraform taint aws_instance.node2
+// terraform plan
